5b70aaf3cb34989d97ac6da5f54111afae8243df
epochlab <james@epochlab.xyz>
2021-07-04 00:32:37 +0100

    Add files via upload

--- train.py
+++ train.py
@@ -95,15 +95,6 @@ generator_optimizer = Adam(0.0002, 0.5)
 discriminator_optimizer = Adam(0.0002, 0.5)
 
 mean_squared_error = tf.keras.losses.MeanSquaredError()
-# binary_cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=False)
-#
-# def generator_loss(sr_out):
-#     return binary_cross_entropy(tf.ones_like(sr_out), sr_out)
-#
-# def discriminator_loss(hr_out, sr_out):
-#     hr_loss = binary_cross_entropy(tf.ones_like(hr_out), hr_out)
-#     sr_loss = binary_cross_entropy(tf.zeros_like(sr_out), sr_out)
-#     return hr_loss + sr_loss
 
 @tf.function
 def content_loss(hr, sr):
