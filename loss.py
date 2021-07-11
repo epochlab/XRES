c16a6171783bcb800429c89a072ee8a7f9a25d48
epochlab <james@epochlab.xyz>
2021-07-11 18:19:24 +0100

    Add files via upload

--- loss.py
+++ loss.py
@@ -1,7 +1,6 @@
+#!/usr/bin/env python3
+
 import tensorflow as tf
-# from model import build_vgg
-#
-# vgg = build_vgg()
 
 mean_squared_error = tf.keras.losses.MeanSquaredError()
 binary_cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=False)
@@ -13,11 +12,3 @@ def discriminator_loss(hr_out, sr_out):
     hr_loss = binary_cross_entropy(tf.ones_like(hr_out), hr_out)
     sr_loss = binary_cross_entropy(tf.zeros_like(sr_out), sr_out)
     return hr_loss + sr_loss
-
-# # @tf.function
-# def content_loss(hr, sr):
-#     sr = tf.keras.applications.vgg19.preprocess_input(sr)
-#     hr = tf.keras.applications.vgg19.preprocess_input(hr)
-#     sr_features = vgg(sr) / 12.75
-#     hr_features = vgg(hr) / 12.75
-#     return mean_squared_error(hr_features, sr_features)
